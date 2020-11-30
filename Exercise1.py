95a42fb35b0ab96f34042a61cbd22e8515c544f2
圖書資訊中心 校務系統組 徐佐壹 職員 <fz083@licc.ad>
2020-11-30 14:58:03 +0800

    FB文章抓取練習

--- Exercise1.py
+++ Exercise1.py
@@ -36,7 +36,6 @@ for x in range(1, 4):
 soup = BeautifulSoup(chrome.page_source, 'html.parser')
 contents = soup.find_all('div', {'class': 'pybr56ya dati1w0a hv4rvrfc n851cfcs btwxx1t3 j83agx80 ll8tlv6m'})
 
-
 for content in contents:
     print(content.find_all('div', {'class': 'kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q'}).getText())
     
